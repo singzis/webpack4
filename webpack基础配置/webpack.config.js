82ccbd9d3d2130a48e2ba3258b7f543dbea55b94
AtoZ <599106418@qq.com>
2019-06-28 18:15:40 +0800

    Update webpack.config.js

--- webpack基础配置/webpack.config.js
+++ webpack基础配置/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
