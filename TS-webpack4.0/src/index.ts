f83952dab845abe3153db3aba05163682084cdfd
zissou72 <599106418@qq.com>
2019-10-24 22:55:11 +0800

    feat:TS的webpack配置与学习

---
+++ TS-webpack4.0/src/index.ts
@@ -0,0 +1,29 @@
+class Student {
+  fullName: string;
+  constructor(public firstName:string, public middleInitial:string, public lastName:string) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User2");
+
+document.body.innerHTML = greeter(user);
+function ff(...arg:any){
+  console.log(arg);
+}
+
+ff({
+  a:1,
+  b:2
+},{
+  b:3,
+  d:4
+})
