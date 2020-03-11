f25e65f640b739932830641593515801cb2bdf3a
singz72 <singzochn@gmail.com>
2020-03-11 15:29:17 +0800

    Update webpack.config.js
    更新webpack4配置文件

--- React-webpack4.0/webpack.config.js
+++ React-webpack4.0/webpack.config.js
@@ -1,45 +1,68 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const DashboardPlugin = require("webpack-dashboard/plugin");
 
 module.exports = {
-    entry: path.join(__dirname, './src/index.js'),
-    devServer: {
-        port: 3000
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        }, {
-            test: /\.css$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                'css-loader',
-                'postcss-loader'
-            ]
-        }, {
-            test: /\.html$/,
-            use: 'html-loader'
-        }, {
-            test: /\.(jpg|svg|png|gif)$/,
-            use: 'file-loader'
-        }]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'index.css'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './public/index.html'
-        }),
-        new CleanWebpackPlugin()
+  entry: path.join(__dirname, "/src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "build/page"),
+    filename: "[name].js"
+  },
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: "all"
+  //   }
+  // },
+  devServer: {
+    port: 3001
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "less-loader"
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images"
+            }
+          }
+        ]
+      }
     ]
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "index.css"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./public/index.html"
+    }),
+    new CleanWebpackPlugin(),
+    new DashboardPlugin()
+  ]
+};
